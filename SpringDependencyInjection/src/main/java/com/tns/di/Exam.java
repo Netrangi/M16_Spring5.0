c0dfe61c3949713dc9607bc6e26d18b2f6580c89
Netra <Netra@DESKTOP-DSM5VNG>
2022-06-20 15:01:01 +0530

    First Commit

---
+++ SpringDependencyInjection/src/main/java/com/tns/di/Exam.java
@@ -0,0 +1,20 @@
+package com.tns.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam {
+
+	public static void main(String[] args) {
+	
+		ApplicationContext c =new ClassPathXmlApplicationContext("beans.xml");
+		Student s1=c.getBean("s1",Student.class);
+		s1.print();
+		/*Student s=c.getBean("s",Student.class);
+		s.print();*/
+
+
+
+	}
+
+}
