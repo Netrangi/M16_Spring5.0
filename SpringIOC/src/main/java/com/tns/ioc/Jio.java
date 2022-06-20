dc353e42e42e293420c8c4e63e769a52d4082a63
Netra <Netra@DESKTOP-DSM5VNG>
2022-06-20 15:00:18 +0530

    First Commit

---
+++ SpringIOC/src/main/java/com/tns/ioc/Jio.java
@@ -0,0 +1,21 @@
+package com.tns.ioc;
+
+public class Jio implements Sim {
+	
+	public Jio() {
+		System.out.println("Jio class constructor");
+
+	}
+
+	public void calling() {
+		System.out.println("Jio calling");
+		
+	}
+
+	public void data() {
+		System.out.println("Jio Data-pack");
+		
+	}
+	
+
+}
